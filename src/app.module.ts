6c98e73dbfef7a2e76079788675476c4739fef83
Xyphuz <hansen0715.2@gmail.com>
2022-09-28 09:17:56 +0800

    chore: delete unnecessary imports in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
-import { PrismaModule } from "nestjs-prisma";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { HistoricalDataModule } from "./historical-data/historical-data.module";
-import { PrismaService } from "./prisma.service";
 import { TickerModule } from "./ticker/ticker.module";
 
 @Module({
