e7fccabbb5786d516b67d9c1db30174ef4823ceb
ppodds <oscar20020629@gmail.com>
2022-10-18 19:01:41 +0800

    feat: add gains chart

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@
     <ClientOnly>
       <Editor ref="editor" />
       <button @click="backtest()">Backtest</button>
+      <button @click="showChart()">Show Chart</button>
+      <Chart v-if="chartVisable" :data="chartData" />
     </ClientOnly>
   </div>
 </template>
@@ -22,6 +24,8 @@ const { data: stockData } = await useFetch<Ticker>(
   `${config.public.apiBaseUrl}/tickers/2330.TW/historical-data?start=1980-01-01`,
 );
 const editor = ref<InstanceType<typeof Editor>>();
+const chartVisable = ref(false);
+const chartData = ref(null);
 
 // prepare runtime
 const stock = new Stock(stockData.value);
@@ -63,5 +67,22 @@ function backtest(): Backtest {
   const t = { gains, winRate, result };
   console.log(t);
   return t;
+}
+
+function showChart() {
+  const result = backtest();
+  chartData.value = {
+    labels: stock.dates,
+    datasets: [
+      {
+        label: "Gain",
+        data: result.gains,
+        borderColor: "rgba(54, 162, 235, 1)",
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        borderWidth: 1,
+      },
+    ],
+  };
+  chartVisable.value = true;
 }
 </script>
