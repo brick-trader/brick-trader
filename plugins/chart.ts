6169496d14c48e0711145df1b3c591e92a7cc49e
ppodds <oscar20020629@gmail.com>
2022-10-22 23:21:30 +0800

    fix: wrong default input value in dashboard

--- plugins/chart.ts
+++
@@ -1,7 +0,0 @@
-import { Chart, registerables } from "chart.js";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook("app:beforeMount", () => {
-    Chart.register(...registerables);
-  });
-});
