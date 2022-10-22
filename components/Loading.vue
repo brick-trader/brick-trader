073c8ee5e4f0dcbf8f80df7fb8866e63a9aeec79
Zheng Zhi Yuan <kevinjone25@g.ncu.edu.tw>
2022-10-22 17:12:04 +0800

    feat: add loading animation in Loading.vue

---
+++ components/Loading.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="animation">
+    <span :style="styles" class="spinner spinner--rotate-square4">
+      <span class="loader-inner"></span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      default: "250px",
+    },
+    color: {
+      default: "#41b883",
+    },
+  },
+  computed: {
+    styles() {
+      return {
+        width: this.size,
+        height: this.size,
+        "--bg-color": this.color,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.animation {
+  background-color: transparent;
+  width: auto;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.spinner {
+  position: relative;
+  border: 4px solid var(--bg-color);
+  animation: loader 2s infinite ease;
+}
+.spinner * {
+  line-height: 0;
+  box-sizing: border-box;
+}
+.loader-inner {
+  vertical-align: top;
+  display: inline-block;
+  width: 100%;
+  background-color: var(--bg-color);
+  animation: loader-inner 2s infinite ease-in;
+}
+@keyframes loader {
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(180deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  75% {
+    transform: rotate(360deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+@keyframes loader-inner {
+  0% {
+    height: 0%;
+  }
+  25% {
+    height: 0%;
+  }
+  50% {
+    height: 100%;
+  }
+  75% {
+    height: 100%;
+  }
+  100% {
+    height: 0%;
+  }
+}
+</style>
