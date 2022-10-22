87f27580b8c9b44cc3b61139d10c124501a3279c
Xyphuz <hansen0715.2@gmail.com>
2022-10-22 10:20:26 +0800

    feat: implement custom macd

---
+++ plugins/blockly/runtime/fn/macd.ts
@@ -0,0 +1,10 @@
+import indicatorts from "indicatorts";
+
+export function macd(closings: number[]): number[] {
+  const ema12 = indicatorts.ema(12, closings);
+  const ema26 = indicatorts.ema(26, closings);
+  const macdLine = indicatorts.subtract(ema12, ema26);
+  const signalLine = indicatorts.ema(9, macdLine);
+
+  return indicatorts.subtract(macdLine, signalLine);
+}
